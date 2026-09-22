9d0133988014805ff6b608d54ea439ea059bb837
Ajimaru <Ajimaru@users.noreply.github.com>
2026-09-22 09:43:34 +0200

    fix: Add API route guards for permissions and handle 403 errors in API client

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_PrintJobHistoryExtended"
 plugin_name = "Print Job History Extended"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "1.18.0.dev27"
+plugin_version = "1.18.0.dev28"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
